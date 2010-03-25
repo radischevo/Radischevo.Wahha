4964c2e19bdb250ceeb29278a41c9526cba0f645
Sergey Rodyushkin <contact@sergey.ro>
2010-03-25 09:14:41 +0000

    git-svn-id: http://svn.sergey.ro:8080/svn/Radischevo.Wahha@42 e161eb7d-40b9-5b48-9704-e6db4491c3f1

--- Framework/Radischevo.Wahha.Core/Extensions/ValueSetExtensions.cs
+++ Framework/Radischevo.Wahha.Core/Extensions/ValueSetExtensions.cs
@@ -19,17 +19,34 @@ namespace Radischevo.Wahha.Core
 
 		public static bool ContainsAll(this IValueSet values, params string[] keys)
 		{
+			return ContainsAll(values, StringComparer.OrdinalIgnoreCase, keys);
+		}
+
+		public static bool ContainsAll(this IValueSet values, 
+			IEqualityComparer<string> comparer, params string[] keys)
+		{
 			foreach (string key in keys)
-				if (!values.Keys.Contains(key))
+				if (!values.Keys.Contains(key, comparer))
 					return false;
 
 			return true;
 		}
 
+		public static bool ContainsAny(this IValueSet values)
+		{
+			return values.Keys.Any();
+		}
+
 		public static bool ContainsAny(this IValueSet values, params string[] keys)
 		{
+			return ContainsAny(values, StringComparer.OrdinalIgnoreCase, keys);
+		}
+
+		public static bool ContainsAny(this IValueSet values, 
+			IEqualityComparer<string> comparer, params string[] keys)
+		{
 			foreach (string key in keys)
-				if (values.Keys.Contains(key))
+				if (values.Keys.Contains(key, comparer))
 					return true;
 
 			return false;
