3d3ce4df505a1dfc1fb93f78e0208f0eabe5f77c
Sergey Rodyushkin <contact@sergey.ro>
2010-09-28 06:50:37 +0000

    git-svn-id: http://svn.sergey.ro:8080/svn/Radischevo.Wahha@71 e161eb7d-40b9-5b48-9704-e6db4491c3f1

--- Framework/Radischevo.Wahha.Data/KeyedDbRepository.cs
+++ Framework/Radischevo.Wahha.Data/KeyedDbRepository.cs
@@ -11,57 +11,37 @@ namespace Radischevo.Wahha.Data
 		DbRepository<TEntity, TKey>
 		where TEntity : class
 	{
-		#region Nested Types
-		private class CachedDbEntry
-		{
-			#region Instance Fields
-			private TEntity _entity;
-			private bool _complete;
-			#endregion
-
-			#region Constructors
-			public CachedDbEntry(TEntity entity, bool complete)
-			{
-				_entity = entity;
-				_complete = complete;
-			}
-			#endregion
-
-			#region Instance Properties
-			public TEntity Entity
-			{
-				get
-				{
-					return _entity;
-				}
-			}
-
-			public bool Complete
-			{
-				get
-				{
-					return _complete;
-				}
-			}
-			#endregion
-		}
-		#endregion
-
 		#region Instance Fields
 		private IDbMaterializer<TEntity> _materializer;
 		private IDbDataProvider _dataProvider;
 		#endregion
 
 		#region Constructors
+		/// <summary>
+		/// Creates a new instance of the 
+		/// <see cref="Radischevo.Wahha.Data.KeyedDbRepository{TKey,TEntity}"/> class.
+		/// </summary>
+		/// <param name="materializer">The <see cref="Radischevo.Wahha.Data.IDbMaterializer{TEntity}"/>
+		/// used to transform database query results into objects.</param>
 		protected KeyedDbRepository(IDbMaterializer<TEntity> materializer)
 			: this(null, materializer)
 		{
 		}
 
+		/// <summary>
+		/// Creates a new instance of the 
+		/// <see cref="Radischevo.Wahha.Data.KeyedDbRepository{TKey,TEntity}"/> class.
+		/// </summary>
+		/// <param name="dataProvider">The <see cref="Radischevo.Wahha.Data.IDbDataProvider"/> 
+		/// used to perform database queries.</param>
+		/// <param name="materializer">The <see cref="Radischevo.Wahha.Data.IDbMaterializer{TEntity}"/>
+		/// used to transform database query results into objects.</param>
 		protected KeyedDbRepository(IDbDataProvider dataProvider,
 			IDbMaterializer<TEntity> materializer)
 		{
 			Precondition.Require(materializer, () => Error.ArgumentNull("materializer"));
+
+			Dictionary<string, object> d = new Dictionary<string, object>();
 
 			_dataProvider = dataProvider;
 			_materializer = materializer;
@@ -69,6 +49,9 @@ namespace Radischevo.Wahha.Data
 		#endregion
 
 		#region Instance Properties
+		/// <summary>
+		/// Gets the initial size of the entity collections.
+		/// </summary>
 		protected virtual int SelectBufferSize
 		{
 			get
@@ -77,6 +60,10 @@ namespace Radischevo.Wahha.Data
 			}
 		}
 
+		/// <summary>
+		/// Gets or sets the <see cref="Radischevo.Wahha.Data.IDbMaterializer{TEntity}"/>
+		/// used to transform database query results into objects.
+		/// </summary>
 		public IDbMaterializer<TEntity> Materializer
 		{
 			get
@@ -90,6 +77,11 @@ namespace Radischevo.Wahha.Data
 			}
 		}
 
+		/// <summary>
+		/// Gets or sets the <see cref="Radischevo.Wahha.Data.IDbDataProvider"/> 
+		/// used to perform database queries. If no data provider is set, 
+		/// the default instance will be used.
+		/// </summary>
 		public IDbDataProvider DataProvider
 		{
 			get
@@ -107,28 +99,48 @@ namespace Radischevo.Wahha.Data
 		#endregion
 
 		#region Instance Methods
+		/// <summary>
+		/// When overridden in a derived class, creates a 
+		/// <see cref="Radischevo.Wahha.Data.DbCommandDescriptor"/> used to 
+		/// select a single entity by its unique key.
+		/// </summary>
+		/// <param name="key">The unique key of the entity.</param>
 		protected abstract DbCommandDescriptor CreateEntitySelector(TKey key);
 
-		protected abstract TKey ExtractKey(IValueSet values);
+		/// <summary>
+		/// When overridden in a derived class, gets the unique key 
+		/// value of the provided entity.
+		/// </summary>
+		/// <param name="entity">The entity to extract key from.</param>
+		protected abstract TKey ExtractKey(TEntity entity);
 
-		protected virtual TEntity Materialize(IValueSet values, bool isComplete)
+		/// <summary>
+		/// Creates and materializes a new entity object using the 
+		/// provided <paramref name="values">data source</paramref>.
+		/// </summary>
+		/// <param name="values">An <see cref="Radischevo.Wahha.Core.IValueSet"/> 
+		/// containing the data for entity materialization.</param>
+		protected virtual TEntity Materialize(IValueSet values)
 		{
-			if (EnableCaching)
-			{
-				TKey key = ExtractKey(values);
-
-				CachedDbEntry instance = Cache.Get(CreateCacheKey(key),
-					() => new CachedDbEntry(_materializer.Materialize(values), isComplete),
-					DateTime.Now.Add(ExpirationTimeout), Tags.ToArray());
-
-				if (isComplete && !instance.Complete)
-					_materializer.Materialize(instance.Entity, values);
-
-				return instance.Entity;
-			}
 			return _materializer.Materialize(values);
 		}
 
+		/// <summary>
+		/// Executes the select query and updates the 
+		/// data properties of the provided entity.
+		/// </summary>
+		/// <param name="entity">The entity to update.</param>
+		public TEntity Load(TEntity entity)
+		{
+			Precondition.Require(entity, () => Error.ArgumentNull("entity"));
+			return ExecuteLoad(entity, CreateEntitySelector(ExtractKey(entity)));
+		}
+
+		/// <summary>
+		/// Executes the select query described by the provided <paramref name="command"/> 
+		/// and transforms the query result into the collection of entities.
+		/// </summary>
+		/// <param name="command">The unique key of the entity.</param>
 		protected override IEnumerable<TEntity> ExecuteSelect(DbCommandDescriptor command)
 		{
 			return DataProvider.Execute(command)
@@ -138,7 +150,7 @@ namespace Radischevo.Wahha.Data
 					int count = -1;
 
 					while (reader.Read())
-						list.Add(Materialize(reader, false));
+						list.Add(Materialize(reader));
 
 					if (reader.NextResult() && reader.Read()
 						&& reader.Keys.Count() == 1)
@@ -148,16 +160,21 @@ namespace Radischevo.Wahha.Data
 				});
 		}
 
+		/// <summary>
+		/// Selects a single entity using the provided unique key.
+		/// </summary>
+		/// <param name="key">An object describing the command to execute.</param>
 		protected override TEntity ExecuteSingle(TKey key)
 		{
-			return ExecuteSingle(CreateEntitySelector(key), true);
+			return ExecuteSingle(CreateEntitySelector(key));
 		}
 
-		protected override TEntity ExecuteSingle(DbCommandDescriptor command)
-		{
-			return ExecuteSingle(command, false);
-		}
-
+		/// <summary>
+		/// Executes the select query and updates the data properties of the provided 
+		/// entity using the first element of the result set.
+		/// </summary>
+		/// <param name="entity">The entity to update.</param>
+		/// <param name="command">An object describing the command to execute.</param>
 		protected override TEntity ExecuteLoad(TEntity entity, DbCommandDescriptor command)
 		{
 			return DataProvider.Execute(command).Using(CommandBehavior.CloseConnection)
@@ -171,22 +188,28 @@ namespace Radischevo.Wahha.Data
 				});
 		}
 
-		protected virtual TEntity ExecuteSingle(DbCommandDescriptor command, bool isComplete)
+		/// <summary>
+		/// Executes the select query described by the provided <paramref name="command"/> 
+		/// and transforms the first element of the resultset into entity.
+		/// </summary>
+		/// <param name="command">An object describing the command to execute.</param>
+		protected override TEntity ExecuteSingle(DbCommandDescriptor command)
 		{
 			return DataProvider.Execute(command).Using(CommandBehavior.CloseConnection)
 				.AsDataReader(reader => {
 					if(reader.Read())
-						return Materialize(reader, isComplete);
+						return Materialize(reader);
 					
 					return null;
 				});
 		}
 
-		public override TEntity Single(TKey key, params string[] tags)
-		{
-			return ExecuteSingle(key);
-		}
-
+		/// <summary>
+		/// Performs application-defined tasks associated with freeing, releasing, or
+		/// resetting unmanaged resources.
+		/// </summary>
+		/// <param name="disposing">A value indicating whether 
+		/// the disposal is called explicitly.</param>
 		protected override void Dispose(bool disposing)
 		{
 			if (_dataProvider != null)
