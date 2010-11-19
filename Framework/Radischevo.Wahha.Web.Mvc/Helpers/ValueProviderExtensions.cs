968a5f279b8fa687e97812a836324d60a20fbad1
Sergey Rodyushkin <contact@sergey.ro>
2010-11-19 14:54:42 +0000

    git-svn-id: http://svn.sergey.ro:8080/svn/Radischevo.Wahha@94 e161eb7d-40b9-5b48-9704-e6db4491c3f1

---
+++ Framework/Radischevo.Wahha.Web.Mvc/Helpers/ValueProviderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+using Radischevo.Wahha.Core;
+
+namespace Radischevo.Wahha.Web.Mvc
+{
+	public static class ValueProviderExtensions
+	{
+		#region Extension Methods
+		public static TValue GetValue<TValue>(this IValueProvider provider, string name)
+		{
+			return GetValue<TValue>(provider, name, default(TValue), CultureInfo.CurrentCulture);
+		}
+
+		public static TValue GetValue<TValue>(this IValueProvider provider,
+			string name, TValue defaultValue)
+		{
+			return GetValue<TValue>(provider, name, defaultValue, CultureInfo.CurrentCulture);
+		}
+
+		public static TValue GetValue<TValue>(this IValueProvider provider,
+			string name, TValue defaultValue, CultureInfo culture)
+		{
+			Precondition.Require(provider, () => Error.ArgumentNull("provider"));
+			ValueProviderResult result = provider.GetValue(name);
+			result.Culture = culture;
+
+			return (result == null) ? defaultValue : result.GetValue<TValue>(defaultValue);
+		}
+		#endregion
+	}
+}
